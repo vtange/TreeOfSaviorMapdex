fa06da0b4820b4e638ee8d427e55b8a244d99c28
Victor Tang <victortang1992@gmail.com>
2015-08-14 04:07:34 -0700

    map works

--- files/app.js
+++ files/app.js
@@ -2,7 +2,7 @@
     //start of function
   var app = angular.module('mapDex', []);
   app.controller('mapSelector', function() {
-  this.zones = {
+    this.zones = {
      map_1021: {
          name: "West Siauliai Woods",
          area: "Klaipeda Area",
@@ -855,7 +855,7 @@
          area: "Kallis Province",
          type: "Field",
          level: 196,
-         map: ""
+         map: "map/innerdistrict.jpg"
      },
      map_1701: {
          name: "Sentry Bailey",
@@ -1340,12 +1340,19 @@
          level: 470,
          map: ""
      }
-     }
+     };
+    this.zone = this.zones.map_1021;
+    this.selectZone = function(setZone){
+          this.zone = setZone;
+    };
+    this.Highlighted = function(checkZone){
+          return this.zone === checkZone;
+    };
     }) //end of controller
   app.controller('tabControl', function() {
       this.tab = 1;
       this.selectTab = function(setTab){
-          this.tab = setTab
+          this.tab = setTab;
       };
       this.isSelected = function(checkTab){
           return this.tab === checkTab;
@@ -1356,3 +1363,73 @@
   //end of function
 })();
 
+
+
+/*
+    zones = {
+     map_1021: {
+         name: "West Siauliai Woods",
+         area: "Klaipeda Area",
+         type: "Field",
+         level: 1,
+         map: "map/westsiliulai.jpg"
+     },
+     map_1001: {
+         name: "Klaipeda",
+         area: "Klaipeda Area",
+         type: "City",
+         level: 0,
+         map: "map/klaipeda.jpg"
+     }
+    };
+    
+    zone = zones.map_1021;
+    selectZone = function(setZone){
+          zone = this.zones[setZone];
+    };
+ 
+    console.log(zone.name);
+    selectZone("map_1001");
+    console.log(zone.name);  
+    */
+/*
+var Good = function() {
+    this.zones = {
+     map_1021: {
+         name: "West Siauliai Woods",
+         area: "Klaipeda Area",
+         type: "Field",
+         level: 1,
+         map: "map/westsiliulai.jpg"
+     },
+     map_1001: {
+         name: "Klaipeda",
+         area: "Klaipeda Area",
+         type: "City",
+         level: 0,
+         map: "map/klaipeda.jpg"
+     },
+     map_1032: {
+         name: "East Siauliai Woods",
+         area: "Klaipeda Area",
+         type: "Field",
+         level: 6,
+         map: "map/eastsiliulai.jpg"
+     }
+     };
+    this.zone = this.zones.map_1021;
+    this.selectZone = function(setZone){
+          this.zone = this.zones[setZone];
+    };
+    this.Highlighted = function(checkZone){
+          return this.zone === this.zones[checkZone];
+    };
+    };
+    var mapp = new Good();
+    
+    
+    
+    mapp.selectZone("map_1001");
+    
+    console.log(mapp.zone.name);
+    */
