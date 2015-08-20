958b0c71cd9cbeb35ac77001859b67f494a8bb26
Victor Tang <victortang1992@gmail.com>
2015-08-20 16:17:33 -0700

    Pre-floors system add

--- files/app.js
+++ files/app.js
@@ -1,19 +1,6 @@
 (function() {
     //start of function
   var app = angular.module('mapDex', []);
-    /*
-app.filter('bossOnly', function() {
-    return function(input) {
-        var out = {};
-        for (var i = 0; i < input.length; i++) {
-            if(input[i].kind == "Boss"){
-                out[key] = input[i];
-            }
-        }
-        return out;
-    }
-});
-*/
   app.controller('mapSelector', function() {
     this.zones = zones;
     this.zone = this.zones.map_1021;
@@ -47,74 +34,3 @@ app.filter('bossOnly', function() {
     }) //end of controller
   //end of function
 })();
-
-
-
-/*
-    zones = {
-     map_1021: {
-         name: "West Siauliai Woods",
-         area: "Klaipeda Area",
-         type: "Field",
-         level: 1,
-         map: "map/westsiliulai.jpg"
-     },
-     map_1001: {
-         name: "Klaipeda",
-         area: "Klaipeda Area",
-         type: "City",
-         level: 0,
-         map: "map/klaipeda.jpg"
-     }
-    };
-    
-    zone = zones.map_1021;
-    selectZone = function(setZone){
-          zone = this.zones[setZone];
-    };
- 
-    console.log(zone.name);
-    selectZone("map_1001");
-    console.log(zone.name);  
-    */
-/*
-var Good = function() {
-    this.zones = {
-     map_1021: {
-         name: "West Siauliai Woods",
-         area: "Klaipeda Area",
-         type: "Field",
-         level: 1,
-         map: "map/westsiliulai.jpg"
-     },
-     map_1001: {
-         name: "Klaipeda",
-         area: "Klaipeda Area",
-         type: "City",
-         level: 0,
-         map: "map/klaipeda.jpg"
-     },
-     map_1032: {
-         name: "East Siauliai Woods",
-         area: "Klaipeda Area",
-         type: "Field",
-         level: 6,
-         map: "map/eastsiliulai.jpg"
-     }
-     };
-    this.zone = this.zones.map_1021;
-    this.selectZone = function(setZone){
-          this.zone = this.zones[setZone];
-    };
-    this.Highlighted = function(checkZone){
-          return this.zone === this.zones[checkZone];
-    };
-    };
-    var mapp = new Good();
-    
-    
-    
-    mapp.selectZone("map_1001");
-    
-    console.log(mapp.zone.name);
-    */
