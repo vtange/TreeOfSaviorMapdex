e029a8c61c3d1606414793346fda9186d2552137
Victor Tang <victortang1992@gmail.com>
2015-08-26 16:07:05 -0700

    noBosses simple filter

--- files/filters.js
+++ files/filters.js
@@ -2,6 +2,18 @@
     //start of function
 var filters=angular.module('filters', []);
 
+filters.filter('noBosses', function() {
+    return function(input) {
+        var out = [];
+        for (var j = 0; j < input.length; j++) {
+            if(input[j].kind == "Normal"){
+                out.push(input[j]);
+            }
+        }
+        return out;
+    }
+});
+    
 filters.filter('matchWith', function() {
     return function(input, searchSubmit) {
         var out = [];
