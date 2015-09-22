e363745731d59ffc6de5af650f683b9531ca8b7a
Victor Tang <victortang1992@gmail.com>
2015-09-21 17:06:46 -0700

    floors now reset back to 1 when click on new zone

--- files/app.js
+++ files/app.js
@@ -7,19 +7,29 @@
     MonsterDB.modelObject = {};
     return MonsterDB;
   }); //end of service
-  app.controller('mapSelector', function() {
+  app.factory('UiDB', function() {
+    var UiDB = {};
+    UiDB.floor = 1;
+    return UiDB;
+  }); //end of service
+  app.controller('mapSelector',['$scope','UiDB', function($scope, UiDB) {
+//load floors from UiDB
+    $scope.UiDB = UiDB;
 //load zones
     this.zones = zones;
 //map selector
     this.zone = this.zones.map_1021;
     this.selectZone = function(setZone){
           this.zone = setZone;
+          $scope.UiDB.floor = 1;
     };
     this.Highlighted = function(checkZone){
           return this.zone === checkZone;
     };
-    }) //end of controller
-  app.controller('tabControl', function() {
+    }]) //end of controller
+  app.controller('tabControl',['$scope','UiDB', function($scope, UiDB) {
+//load floors from UiDB
+    $scope.UiDB = UiDB;
       //tabs
       this.tab = 1;
       this.selectTab = function(setTab){
@@ -42,14 +52,13 @@
           return this.bossToggle === 1;
       };
       //floor changing
-      this.floor = 1;
       this.changeFloor = function(newFloor){
-          this.floor = newFloor;
+          $scope.UiDB.floor = newFloor;
       };
       this.currentFloor = function(isFloor){
-          return this.floor === isFloor;
+          return $scope.UiDB.floor === isFloor;
       };
-    }) //end of controller
+    }]) //end of controller
   app.controller('monsterDb',['$scope','MonsterDB', function($scope, MonsterDB) {
 //monsterDb -> get database, get model object
     $scope.MonsterDB = MonsterDB;
